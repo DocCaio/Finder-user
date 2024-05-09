c7de3169dc3db91a17c56ee5465fedf504c34441
Caio <108128119+DocCaio@users.noreply.github.com>
2024-05-09 18:38:44 -0300

    up

---
+++ src/main.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+import { createBrowserRouter , RouterProvider } from 'react-router-dom';
+
+//Pages
+
+import Home from './routes/Home.tsx';
+
+const router = createBrowserRouter([
+
+  {
+    path: "/",
+    element:<App/>,
+    children: [
+
+      {
+        path: "/",
+        element: <Home/>,
+      },
+
+    ],
+  },
+
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
