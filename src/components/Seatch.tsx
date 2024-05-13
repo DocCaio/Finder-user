bd0c01c8f85ef775face1b2e5a0cb96109915a94
Caio <108128119+DocCaio@users.noreply.github.com>
2024-05-13 19:03:39 -0300

    up

--- src/components/Seatch.tsx
+++ src/components/Seatch.tsx
@@ -1,15 +1,27 @@
-import { BsSearch } from "react-icons/bs";
-//import React from "react";
+type SearchProps = {
+  loadUser: (userName: string) => Promise<void>;
+  
+};
 
-const Search = () => {
+import { useState } from "react";
+import { BsSearch } from "react-icons/bs";
+
+
+const Search = ({loadUser}: SearchProps) => {
+
+  const [userName , setUserName] = useState("");
     return (
         <div>
           
       <h2>Busque pelo seu usuário:</h2>
       <p>Veja seus repositórios</p>
       <div>
-        <input type="text" placeholder="Digite seu nick"/>
-        <button>
+        <input 
+        type="text" 
+        placeholder="Digite seu nick"
+        onChange={(e) => setUserName(e.target.value)}
+        />
+        <button onClick={() => loadUser(userName)}>
             <BsSearch/>
         </button>
       </div>
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,12 +1,32 @@
+import { UserProps } from "./types/user";
+
 import Search from "../components/Seatch";
 import { useState } from 'react';
 
 const Home = () => {
 
-    const [user,setUser] = useState(null);
+    const [user,setUser] = useState<UserProps | null>(null);
+
+    const loadUder = async(userName: string) => {
+
+        const res = await fetch(`https://api.github.com/users/${userName}`)
+        const data = await res.json();
+
+        const {avatar_url , login, location , followers , following} = data
+
+         const userData: UserProps = {
+            avatar_url , 
+            login, 
+            location , 
+            followers , 
+            following
+         }
+
+         setUser(userData);
+    }
     return (
         <div>
-            <Search/>
+            <Search loadUser={loadUder}/>
         </div>
     )
 }
